d31a0e8ea19abced4689e936202d636c3d9148ec
Ankitaben Hareshbhai Patel <ankita.it.2012@gmail.com>
2019-02-19 15:15:44 +0800

    new changes

--- features/step_definitions/steps.js
+++ features/step_definitions/steps.js
@@ -1,4 +1,13 @@
 var assert = require('assert');
+var fs = require("fs");
+var Canvas = require("canvas");
+var jdenticon = require("jdenticon");
+
+
+    // var canvas = new canvas(200, 200, "png");
+    // var g = canvas.getContext("2d");
+    // g.fillStyle = "black";
+    // g.fillRect(0, 0, 100, 100);
 
 module.exports = function () {
 
@@ -7,7 +16,31 @@ module.exports = function () {
   });
 
   this.When(/^I have entered "([^"]*)" and "([^"]*)" into the calculator, Substraction$/, function ( number1, number2 ) {
-  	return this.driver.switchTo().activeElement().sendKeys(number1+"-"+number2+"=");
+  	// return this.driver.switchTo().activeElement().sendKeys(number1+"-"+number2+"=");
+
+
+    // var buf = canvas.toBuffer();
+    // fs.writeFileSync("test.png", buf);
+
+
+    // var pngfile = fs.createWriteStream("./testicon.png"),
+    //   stream = canvas.pngStream();
+    //     stream.on("data", function(chunk){
+    //       console.log("not save png");
+    //         pngfile.write(chunk);
+    //     });
+    //     stream.on("end", function(){
+    //         console.log("saved png");
+    //     });
+
+        const fs = require('fs')
+      const out = fs.createWriteStream(../reports + '/test.png')
+      const stream = canvas.createPNGStream()
+      stream.pipe(out)
+      out.on('finish', () =>  console.log('The PNG file was created.'))
+
+            return this.driver.switchTo().activeElement().sendKeys(number1+"-"+number2+"=");
+
   });
 
   this.Then(/^subsctraction value is$/, function () {
--- features/support/world.js
+++ features/support/world.js
@@ -1,5 +1,4 @@
 var seleniumWebdriver = require('selenium-webdriver');
-var firefox = require('selenium-webdriver/firefox');
 var chrome = require('selenium-webdriver/chrome');
 
 until = seleniumWebdriver.until;
@@ -12,92 +11,9 @@ function CustomWorld() {
 
 }
 
-
 module.exports = function() {
   this.World = CustomWorld;
   this.setDefaultTimeout(30 * 1000);
 };
 
 
-
-
-// var wd = require('wd');
-// var bs = require('../../bootstrap');
-// var xpath = require('xpath');
-// var id = require('id');
-// var log = bs.log;
-// var browser;
-// var asserters = wd.asserters;
-// var tn = require('../../testenvironment.json');
-
-//xapths
-// var registrationXpath="//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIAButton[1]";
-// var emailXapth="//UIAApplication[1]/UIAWindow[1]/UIAStaticText[1]";
-// var passowrdXpath="//UIAApplication[1]/UIAWindow[1]/UIAStaticText[2]";
-// var loginXpath="//UIAApplication[1]/UIAWindow[1]/UIAButton[3]";
-
-// var buttonID="canvas";
-
-
-// var RegistrationPage = function(theBrowser) {
-//     log.info("Creating Tour Page object");
-
-//     this.browser = theBrowser;
-//     browser = theBrowser;
-// };
-
-// var driver;
-
-// //click on registration page
-// RegistrationPage.prototype.clickonregistration= function(done) {
-//     browser.waitForElementByID(buttonID,function(err, el){
-//         if(err) {
-//             done(err);
-//         }else{
-//             el.click(function(err){
-//                 if(err)
-//                 {
-//                     done(err);
-//                 }else{
-//                     done();
-//                 }
-//             });
-//         }
-//     });
-// }
-
-// RegistrationPage.prototype.clickonpassword= function(done) {
-//     browser.waitForElementByXPath(passowrdXpath,function(err, el2){
-//         if(err) {
-//             done(err);
-//         }else{
-//             el2.type("Ab1234",function(err){
-//                 if(err)
-//                 {
-//                     done(err);
-//                 }else{
-//                     done();
-//                 }
-//             });
-//         }
-//     });
-// }
-
-// //click on registration page
-// RegistrationPage.prototype.clickonloginbutton= function(done) {
-//     browser.waitForElementByXPath(loginXpath,function(err, el){
-//         if(err) {
-//             done(err);
-//         }else{
-//             el.click(function(err){
-//                 if(err)
-//                 {
-//                     done(err);
-//                 }else{
-//                     done();
-//                 }
-//             });
-//         }
-//     });
-// }
-// module.exports = RegistrationPage;
